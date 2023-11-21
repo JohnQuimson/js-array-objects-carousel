7185286b2036b555ba773c36a3654f3bdba2f232
JohnQuimson <john.quimson777@gmail.com>
2023-11-21 20:32:31 +0100

    feat: eventlistener next

--- js/main.js
+++ js/main.js
@@ -81,3 +81,32 @@ for (let i = 0; i < arrayLength; i++) {
 
   raccoglitore.append(liElement);
 }
+
+const domSlides = document.querySelectorAll('.item');
+console.log(domSlides);
+
+/*
+-------------------------
+Azioni al click del pulsante next
+-------------------------
+ */
+next.addEventListener('click', function () {
+  //Di default rimuovo la classe active al primo elemento
+  domSlides[slideAttivaIndex].classList.remove('active');
+  //Di default rimuovo la classe active-thumbnails al primo elemento
+  // domThumbnails[slideAttivaIndex].classList.remove('active-thumbnails');
+  //se il contatore del NodeList è minore del num di elementi dei NodeList, crementa il contatore
+  if (slideAttivaIndex < domSlides.length - 1) {
+    slideAttivaIndex++;
+    //se il contatore supera il limite, torna da capo
+  } else {
+    slideAttivaIndex = 0;
+  }
+  domSlides[slideAttivaIndex].classList.add('active');
+  console.log('next');
+  console.log(`Immgine con 'active': domSlides[${slideAttivaIndex}]`);
+  // domThumbnails[slideAttivaIndex].classList.add('active-thumbnails');
+  console.log(
+    `Immgine con 'active-thumbnails': domThumbnails[${slideAttivaIndex}]`
+  );
+});
