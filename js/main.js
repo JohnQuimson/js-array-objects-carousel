03abf3673f9fa98d31690a42b5e5448323978521
JohnQuimson <john.quimson777@gmail.com>
2023-11-21 18:56:05 +0100

    feat: aggiunta img con arr obj

--- js/main.js
+++ js/main.js
@@ -1,187 +1,83 @@
 'use strict';
 
-const arrayImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
-const slider = document.querySelector('.items');
+const arrayObjectCarousel = [
+  {
+    foto: '01.jpg',
+    titolo: 'Titolo 1',
+    descrizione:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quas nihil, eveniet omnis illo repudiandae incidunt consequuntur aliquam doloremque, beatae nostrum cumque facere natus perspiciatis architecto odio consequatur ut reprehenderit.',
+  },
+  {
+    foto: '02.jpg',
+    titolo: 'Titolo 2',
+    descrizione:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quas nihil, eveniet omnis illo repudiandae incidunt consequuntur aliquam doloremque, beatae nostrum cumque facere natus perspiciatis architecto odio consequatur ut reprehenderit.',
+  },
+  {
+    foto: '03.jpg',
+    titolo: 'Titolo 3',
+    descrizione:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quas nihil, eveniet omnis illo repudiandae incidunt consequuntur aliquam doloremque, beatae nostrum cumque facere natus perspiciatis architecto odio consequatur ut reprehenderit.',
+  },
+  {
+    foto: '04.jpg',
+    titolo: 'Titolo 4',
+    descrizione:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quas nihil, eveniet omnis illo repudiandae incidunt consequuntur aliquam doloremque, beatae nostrum cumque facere natus perspiciatis architecto odio consequatur ut reprehenderit.',
+  },
+  {
+    foto: '05.jpg',
+    titolo: 'Titolo 5',
+    descrizione:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias quas nihil, eveniet omnis illo repudiandae incidunt consequuntur aliquam doloremque, beatae nostrum cumque facere natus perspiciatis architecto odio consequatur ut reprehenderit.',
+  },
+];
+let slideAttivaIndex = 0;
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
-const container = document.querySelector('.container');
-const stop = document.querySelector('.stop');
-const play = document.querySelector('.play');
 
-//dichiaro quale slide[i] deve avere la class 'active'
-let slideAttivaIndex = 0;
+// STAMPA IN CONSOLE
+const arrayLength = arrayObjectCarousel.length;
 
-/*
--------------------------
-AGGIUNTA ELEMENTI ALL'HTML
--------------------------
- */
+for (let i = 0; i < arrayLength; i++) {
+  const singleElement = arrayObjectCarousel[i];
 
-//AGGIUNTA DIV CON IMG
-for (let i = 0; i < arrayImages.length; i++) {
-  const elementSlide = document.createElement('div');
-  elementSlide.classList.add('item');
+  console.log(`Element ${i + 1}`);
 
-  if (i === slideAttivaIndex) {
-    elementSlide.classList.add('active');
+  for (let key in singleElement) {
+    console.log(`${key}: ${singleElement[key]}`);
   }
 
-  const elementImg = document.createElement('img');
-  elementImg.src = `img/${arrayImages[i]}`;
-  elementImg.alt = `img ${arrayImages[i + 1]}`;
-
-  slider.append(elementSlide);
-  elementSlide.append(elementImg);
-}
-//AGGIUNTA THUMBNAILS
-//creo un elemento div con classe raccolta-thumbnails dentro container
-const raccoltaThumbnailsDiv = document.createElement('div');
-raccoltaThumbnailsDiv.classList.add('raccolta-thumbnails');
-
-//aggiungo il div al container
-container.append(raccoltaThumbnailsDiv);
-
-//dichiaro una variabile in cui estraggo raccolta-thumbnails
-const raccoltaTn = document.querySelector('.raccolta-thumbnails');
-
-//aggiungo gli elemen img
-for (let i = 0; i < arrayImages.length; i++) {
-  const elementThumbnails = document.createElement('div');
-  elementThumbnails.classList.add('elem-raccolta');
-
-  if (i === slideAttivaIndex) {
-    elementThumbnails.classList.add('active-thumbnails');
+  if (!(i === arrayLength - 1)) {
+    console.log(``);
   }
-
-  const elementImgRaccolta = document.createElement('img');
-  elementImgRaccolta.src = `img/${arrayImages[i]}`;
-  elementImgRaccolta.alt = `img ${arrayImages[i + 1]}`;
-
-  raccoltaTn.append(elementThumbnails);
-  elementThumbnails.append(elementImgRaccolta);
 }
 
-//prendo in considerazione tutti gli elementi con class 'item', i quali vanno a finire in una specie di 'array'
-const domSlides = document.querySelectorAll('.item');
-let lunghDomSlides = domSlides.length - 1;
-//prendo in considerazione tutti gli elementi con class 'elem-raccolta', i quali vanno a finire in una specie di 'array'
-const domThumbnails = document.querySelectorAll('.elem-raccolta');
-let lunghDomThumbnails = domThumbnails.lenght - 1;
+//estrapolo ul
+const raccoglitore = document.querySelector('.raccoglitore');
 
 /*
--------------------------
-Azioni al click del pulsante next
--------------------------
- */
-next.addEventListener('click', function () {
-  //Di default rimuovo la classe active al primo elemento
-  domSlides[slideAttivaIndex].classList.remove('active');
-  //Di default rimuovo la classe active-thumbnails al primo elemento
-  domThumbnails[slideAttivaIndex].classList.remove('active-thumbnails');
-  //se il contatore del NodeList è minore del num di elementi dei NodeList, crementa il contatore
-  if (slideAttivaIndex < lunghDomSlides) {
-    slideAttivaIndex++;
-    //se il contatore supera il limite, torna da capo
-  } else {
-    slideAttivaIndex = 0;
-  }
-  domSlides[slideAttivaIndex].classList.add('active');
-  console.log('next');
-  console.log(`Immgine con 'active': domSlides[${slideAttivaIndex}]`);
-  domThumbnails[slideAttivaIndex].classList.add('active-thumbnails');
-  console.log(
-    `Immgine con 'active-thumbnails': domThumbnails[${slideAttivaIndex}]`
-  );
-});
-
-/*
--------------------------
-Azioni al click del pulsante prev
--------------------------
- */
-prev.addEventListener('click', function () {
-  domSlides[slideAttivaIndex].classList.remove('active');
-  domThumbnails[slideAttivaIndex].classList.remove('active-thumbnails');
-
-  if (slideAttivaIndex > 0) {
-    slideAttivaIndex--;
-  } else {
-    slideAttivaIndex = lunghDomSlides;
-  }
-  domSlides[slideAttivaIndex].classList.add('active');
-  console.log('prev');
-  console.log(`Immgine con 'active': domSlides[${slideAttivaIndex}]`);
-
-  domThumbnails[slideAttivaIndex].classList.add('active-thumbnails');
-
-  console.log(
-    `Immgine con 'active-thumbnails': domThumbnails[${slideAttivaIndex}]`
-  );
-});
-
-/* 
----------------------
-Al Click su un elemento del thumbails, mi mostra quell img
----------------------
+clone element
 */
-//Utilizzo un ciclo for per accedere a tutti gli elementi di domThumbnails, e per ognuno di essi, aggiungo un EventListener 'click'
-for (let i = 0; i < domThumbnails.length; i++) {
-  domThumbnails[i].addEventListener('click', function () {
-    // Rimuovo la classe 'active' e 'active-thumbnails' con indice 0
-    domSlides[slideAttivaIndex].classList.remove('active');
-    domThumbnails[slideAttivaIndex].classList.remove('active-thumbnails');
 
-    // Aggiungo la classe 'active-thumbnails' all'elemento cliccato
-    domThumbnails[i].classList.add('active-thumbnails');
+const templateCarousel = document.getElementById('carousel-template');
 
-    // Aggiorno il contatore degli indici delle slide attive con il contatore i del ciclo for
-    slideAttivaIndex = i;
+for (let i = 0; i < arrayLength; i++) {
+  const singleElement = arrayObjectCarousel[i];
 
-    //Aggiungo e rimuovo 'active' agli item del carosello
-    domSlides[slideAttivaIndex].classList.remove('active');
-    domSlides[slideAttivaIndex].classList.add('active');
+  const liElement = templateCarousel.content.cloneNode(true);
 
-    //stampe in console.log
-    console.log(`Hai cliccato sull'elemento: thumbnails[${i}]`);
-  });
-}
+  liElement.querySelector('.item .photo img').src = `img/${singleElement.foto}`;
+  liElement.querySelector('.item .photo img').alt = singleElement.titolo;
 
-function autoplayNext() {
-  //Di default rimuovo la classe active al primo elemento
-  domSlides[slideAttivaIndex].classList.remove('active');
-  //Di default rimuovo la classe active-thumbnails al primo elemento
-  domThumbnails[slideAttivaIndex].classList.remove('active-thumbnails');
+  liElement.querySelector('.item .name').innerText = singleElement.titolo;
 
-  //se il contatore del NodeList è minore del num di elementi dei NodeList, incrementa il contatore
-  if (slideAttivaIndex < lunghDomSlides) {
-    slideAttivaIndex++;
+  liElement.querySelector('.item .role').innerText = singleElement.descrizione;
 
-    //se il contatore supera il limite, torna da capo
-  } else {
-    slideAttivaIndex = 0;
+  //assegno la classe active all'elem con indice 0
+  if (i === slideAttivaIndex) {
+    liElement.querySelector('.item').classList.add('active');
   }
 
-  domSlides[slideAttivaIndex].classList.add('active');
-
-  console.log(`Immgine attiva: ${slideAttivaIndex + 1}`);
-
-  domThumbnails[slideAttivaIndex].classList.add('active-thumbnails');
+  raccoglitore.append(liElement);
 }
-
-let myFnNext;
-let autoplayActive = false;
-play.addEventListener('click', function () {
-  //condizione per non far attivare una seconda volta il setInterval
-  if (!autoplayActive) {
-    myFnNext = setInterval(autoplayNext, 3_000);
-    autoplayActive = true;
-    console.log('Avviato autoplay');
-  } else {
-    console.log('Non puoi cliccare di nuovo!');
-  }
-});
-stop.addEventListener('click', function () {
-  console.log('Autoplay STOP');
-  autoplayActive = false;
-  clearInterval(myFnNext);
-});
